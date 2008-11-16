03ec0736cd525304a28c6dc8a9bd136c80566d81
Arafat Mohamed <github@arafatm.com>
2008-11-15 19:34:05 -0600

    Scaffold for house

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,4 +1,7 @@
 class HousesController < ApplicationController
+  require 'rubygems'
+  require 'google_geocode'
+
   resources_controller_for :houses
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :houses
+
 
   map.resources :houses
   map.connect ':controller/:action/:id'
