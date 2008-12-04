944cc1476283aa873cc5a97f49366a53248a4f7d
Arafat Mohamed <github@arafatm.com>
2008-12-04 14:23:31 -0500

    Trying to figure out how to write good specs.

--- spec/controllers/houses_controller_spec.rb
+++ spec/controllers/houses_controller_spec.rb
@@ -4,53 +4,40 @@ describe HousesController do
 
   # it_should_behave_like 'a RESTful controller with an index action'
   describe 'handling GET /plural (index)' do
-    it 'should assign houses' do
-      #TODO: Why does 
-      # houses = houses[stub(:House), stub(:House), stub(:House)]
-      #not work?
-      # Output = assigns[:houses] go []
-      houses = [House.generate!, House.generate!, House.generate!]
-      houses.stubs(:find).with(:all).returns(houses)
+
+    it 'should be successful' do
       get :index
-      assigns[:houses].should  == houses
+      response.should be_success
     end
-  end
 
     it 'should render template index' do
       get :index
       response.should render_template('index')
     end
 
-#    it 'should load a google map' do
-#      map = stub(:map)
-#      #TODO: Why does map.expects(:new) not work?
-#      map.stubs(:new)
-#      get :index
-#    end
-#
-#    it 'should create a large google map' do
-#      map = stub(:map)
-#      map.stubs(:control_init).with(:large_map => true, :map_type => true)
-#      get :index
-#    end
-#
-#    it 'should center the map on nashville' do
-#      map = stub(:map)
-#      map.stubs(:center_zoom_init).with([36.058887, -86.782056],10)
-#      get :index
-#    end
-#
-#    it 'should mark all houses on the map' do
-#      map = stub(:map)
-#      map.stubs(:overlay_init)
-#      get :index
-#    end
-#  end
+    it 'should assign houses for the view' do
+      houses = [House.generate!, House.generate!, House.generate!]
+      houses.stubs(:find).with(:all).returns(houses)
+      get :index
+      assigns[:houses].should  == houses
+    end
 
-  #it_should_behave_like 'a RESTful controller with a show action'
-  #TODO it_should_behave_like 'a RESTful controller with a create action'
-  #it_should_behave_like 'a RESTful controller with a new action'
-  #it_should_behave_like 'a RESTful controller with an edit action'
-  #TODO it_should_behave_like 'a RESTful controller with an update action'
-  #it_should_behave_like 'a RESTful controller with a destroy action'
+    it 'should assign a google map' do
+      #TODO: Why does map.expects(:new) not work?
+      get :index
+      assigns[:map].should_not be_nil
+    end
+  end
+
+  describe 'handling POST /singular (create)' do
+    describe 'when successful' do
+      it 'should save the house'
+      it 'should flash a success notice'
+      it 'should redirect to index'
+    end
+    describe 'when unsuccessful' do
+      it 'should flash an error notice'
+      it 'should rerender the new record form'
+    end
+  end
 end
