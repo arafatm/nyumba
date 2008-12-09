4039cf69e88bf023a72d7bc24fbe88f1293fcc15
Arafat Mohamed <github@arafatm.com>
2008-12-09 18:46:59 -0500

    Refactored House.index controller to create a seperate method for assigning gmap

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -7,23 +7,7 @@ class HousesController < ApplicationController
   def index
     self.resources = find_resources
 
-    gg = load_geocode_key
-    @map = GMap.new("map_div")
-    @map.control_init(:large_map => true,:map_type => true)
-    @map.center_zoom_init([36.058887, -86.782056],10)
-
-    self.resources.each do |house|
-      loc = house.geocode.split(' ')
-      info =  "#{house.address}<BR>"
-      info << "(#{house.year}) "
-      info << "$#{house.price} ($#{house.taxes})<BR>"
-      info << "#{house.bedrooms} BR / "
-      info << "#{house.bathrooms} BA "
-      info << "#{house.acres} Acres"
-      @map.overlay_init(GMarker.new([loc[0], loc[1]],
-                                    :title => house.mls, 
-                                    :info_window => info))
-    end
+    gmap(self.resources)
   end
 
   def create
@@ -78,6 +62,26 @@ class HousesController < ApplicationController
     gg = load_geocode_key
     loc = gg.locate(address)
     "#{loc.latitude} #{loc.longitude}"
+  end
+
+  def gmap(houses)
+    gg = load_geocode_key
+    @map = GMap.new("map_div")
+    @map.control_init(:large_map => true,:map_type => true)
+    @map.center_zoom_init([36.058887, -86.782056],10)
+
+    self.resources.each do |house|
+      loc = house.geocode.split(' ')
+      info =  "#{house.address}<BR>"
+      info << "(#{house.year}) "
+      info << "$#{house.price} ($#{house.taxes})<BR>"
+      info << "#{house.bedrooms} BR / "
+      info << "#{house.bathrooms} BA "
+      info << "#{house.acres} Acres"
+      @map.overlay_init(GMarker.new([loc[0], loc[1]],
+                                    :title => house.mls, 
+                                    :info_window => info))
+    end
   end
 
 
